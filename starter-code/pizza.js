6f44d225488902001c4f98fdd1fc852a749d0283
Alfonso Silva <alfsilfer@gmail.com>
2019-01-15 19:03:32 +0100

    bueno

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function () {
+  
+  $('btn')
+  $('.btn-pepperonni').click(function(){
+    $('.pep').toggle();
+  });
+  
+  $('.btn-mushrooms').click(function(){
+    $('.mushroom').toggle();
+  });
+  
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').toggle();
+  });
+  $('.btn-sauce').click(function(){
+    
+    $('.sauce-white').toggle();
+    $('.sauce').toggle();
+  })
+  $('.btn-crust').click(function(){
+    $('.cheese').toggle();
+  })
+});
